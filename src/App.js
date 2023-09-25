9a268303d5789dada21134fc50e04bb721ce19fa
Sumit-singh18 <ss7262692@gmail.com>
2023-09-25 12:50:58 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Action from './Components/redux/action';
+import Home from './Components/Home/Home'
 // import Navbar from './Components/Home/Navbar';
 function App() {
   return (
     <div className="App">
-     <h1><Action/></h1>
+     <h1><Home/></h1>
     </div>
   );
 }
 
 export default App;
+
+
+
